4a0607a7f62c1b27212d0ef8fdf1750d8e4632cb
nma044 <nathanielm@lyse.net>
2022-07-19 19:42:17 +0200

    Started project

---
+++ src/main/java/com/nathanielm/dailylist/DailylistApplication.java
@@ -0,0 +1,13 @@
+package com.nathanielm.dailylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailylistApplication.class, args);
+	}
+
+}
